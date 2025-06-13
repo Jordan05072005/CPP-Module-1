355428e73e3ee136c8d880a1f21535c979f38013
Jordan <guaglio.jordan@gmail.com>
2025-06-13 11:11:45 +0200

    push with -std=c++98

--- mod02/ex01/Fixed.cpp
+++ mod02/ex01/Fixed.cpp
@@ -34,7 +34,6 @@ Fixed& Fixed::operator=(const Fixed& fixed)
 }
 
 int Fixed::getRawBits( void ) const{
-	std::cout << "getRawBits member function called" << std::endl;
 	return (this->number);
 }
 
